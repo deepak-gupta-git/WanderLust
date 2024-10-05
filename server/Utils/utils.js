be5268535221b8e8955ee2c30a0243fb04671ef4
Deepak Gupta <154800280+deepak-gupta-git@users.noreply.github.com>
2024-10-06 00:29:39 +0530

    Update utils.js

--- server/Utils/utils.js
+++ server/Utils/utils.js
@@ -1,15 +1,17 @@
 const mongoose = require("mongoose");
-
 const URI = process.env.URI;
 
 const ConnectDb = async () => {
-    try {
-        await mongoose.connect(URI); 
-        console.log("DB Connected successfully");
-    } catch (error) {
-        console.error("Error connecting to the database:", error.message);
-        throw error;
-    }
+  try {
+    await mongoose.connect(URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("DB Connected successfully");
+  } catch (error) {
+    console.error("Error connecting to the database:", error.message);
+    throw error;
+  }
 };
 
 module.exports = ConnectDb;
