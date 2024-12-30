7e5052feb4d066ead7b4371d9d81c79387b005df
Deepak Gupta <154800280+deepak-gupta-git@users.noreply.github.com>
2024-12-30 13:40:58 +0530

    Update ListingDetails.jsx

--- client/src/Pages/ListingDetails.jsx
+++ client/src/Pages/ListingDetails.jsx
@@ -77,6 +77,7 @@ const ListingDetails = () => {
                 >
                   <button className="px-6 py-2 bg-green-500 text-white font-semibold rounded-lg shadow-md hover:bg-green-600 transition duration-300">
                     Book
+                     onClick={onClick} 
                   </button>
                 </NavLink>
               </div>
