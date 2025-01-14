49697ad7331d555c18e36732f864bef0d3f9e655
Deepak Gupta <154800280+deepak-gupta-git@users.noreply.github.com>
2025-01-14 20:46:54 +0530

    Update Booking.jsx

--- client/src/Pages/Booking.jsx
+++ client/src/Pages/Booking.jsx
@@ -39,7 +39,7 @@ const Booking = () => {
     const receiptId = "1234567890";
 
     try {
-      const response = await fetch("http://localhost:3000/book", {
+      const response = await fetch("https://wander-lust-server.vercel.app/book", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
