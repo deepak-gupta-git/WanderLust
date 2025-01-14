3486adaab0638a1c0fc00360e4cb7b2bb632d0af
deepakgupta <deepak.gupta18122003@gmail.com>
2025-01-14 20:43:53 +0530

    changes the conflict

--- client/src/Pages/ListingDetails.jsx
+++ client/src/Pages/ListingDetails.jsx
@@ -11,16 +11,6 @@ import ReviewList from '../Components/ReviewList';
 
 const ListingDetails = () => {
   const mapContainer = useRef(null);
-
-<<<<<<< HEAD
-=======
-  const onClick = () => {
-    toast.error("You are not the Owner of this listing");
-  };
-
- 
-
->>>>>>> 5e422fe8cc7f5fe14ada383c5ccf7dffd526363d
   const location = useLocation();
   const { state } = location;
   const { price, description, location: loc, image, title, country, geometry  } = state || {};
@@ -30,17 +20,11 @@ const ListingDetails = () => {
 
 
 
-
   const onClick = () => {
     toast.success("You Can Contact To Our Mentor For More Extra Details");
   };
 
   useEffect(() => { 
-
-    // if (!coordinates || coordinates.length !== 2) {
-    //   console.error("Invalid coordinates", coordinates);
-    //   return;
-    // }
     mapboxgl.accessToken = 'pk.eyJ1IjoidGhlY2xhc3Nyb29tIiwiYSI6ImNscnJ6dXo4ajAxYTMya3A0NG55cW1pMjYifQ.oLJ-X878O9NJFYwI_rwiLQ';
 
     const map = new mapboxgl.Map({
@@ -95,28 +79,12 @@ const ListingDetails = () => {
                   </button>
                 </NavLink>
 
-<<<<<<< HEAD
-=======
-                <button 
-                  onClick={onClick}
-                  className="px-6 py-2 bg-red-500 text-white font-semibold rounded-lg shadow-md hover:bg-red-600 transition duration-300" >
-                  Delete
-                </button>
->>>>>>> 5e422fe8cc7f5fe14ada383c5ccf7dffd526363d
                 <NavLink 
                   to="/checkout"
                   state={{ price, title, loc, country, image }} // Pass location details
                 >
-<<<<<<< HEAD
                   <button className="px-6 py-2 bg-red-500 text-white font-semibold rounded-lg shadow-md hover:bg-red-600 transition duration-300">
                      Check it Out
-=======
-                  <button 
-   
-                    className="px-6 py-2 bg-green-500 text-white font-semibold rounded-lg shadow-md hover:bg-green-600 transition duration-300">
-                    Book
-                     
->>>>>>> 5e422fe8cc7f5fe14ada383c5ccf7dffd526363d
                   </button>
                 </NavLink>
               </div>
